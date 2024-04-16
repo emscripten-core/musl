21b82592770f6441cc1c4b2c4ddb1c18a0f6c14b
Sam Clegg <sbc@chromium.org>
2024-04-16 12:50:31 -0700

    Update to emscripten 3.1.57

--- src/stat/fchmodat.c
+++ src/stat/fchmodat.c
@@ -5,7 +5,9 @@
 
 int fchmodat(int fd, const char *path, mode_t mode, int flag)
 {
+#ifndef __EMSCRIPTEN__
 	if (!flag) return syscall(SYS_fchmodat, fd, path, mode);
+#endif
 
 	int ret = __syscall(SYS_fchmodat2, fd, path, mode, flag);
 	if (ret != -ENOSYS) return __syscall_ret(ret);
@@ -32,7 +34,11 @@ int fchmodat(int fd, const char *path, mode_t mode, int flag)
 	ret = stat(proc, &st);
 	if (!ret) {
 		if (S_ISLNK(st.st_mode)) ret = __syscall_ret(-EOPNOTSUPP);
+#ifdef __EMSCRIPTEN__
+		else ret = syscall(SYS_fchmodat2, AT_FDCWD, proc, mode, 0);
+#else
 		else ret = syscall(SYS_fchmodat, AT_FDCWD, proc, mode);
+#endif
 	}
 
 #ifdef __EMSCRIPTEN__
